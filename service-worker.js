85142b337d1a9c093e9902efff8803b099add8e6
Maksymilian Śleziak <297408+maxbmx@users.noreply.github.com>
2026-01-09 17:24:52 +0100

    Optimize iframe loading and cache assets

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,24 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('v1').then(cache =>
-      cache.addAll([
-        '/',
-        '/index.html'
-      ])
+const CACHE_NAME = 'v2';
+const CACHE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/awesome-beskid-camera.svg',
+  '/awesome-beskid-camera.png',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => (key === CACHE_NAME ? null : caches.delete(key))))
     )
   );
 });
